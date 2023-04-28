59528c73ea2d4fbd947a96bf9cc19430349c88be
koenbres04 <koenbres@gmail.com>
2023-04-28 14:17:44 +0200

    Commented some more and git ignored the idea folder.

--- spatial_chem_sim.py
+++ spatial_chem_sim.py
@@ -13,6 +13,10 @@ DIFFUSE_CONVOLVE = np.array([
 
 
 class SpacialDiffEquation(abc.ABC):
+    """"
+    A class for spatial differential equations with diffusion
+    """
+
     def __init__(self, width, height, ds, diffusion_kernel, chemical_count, diffusion_rates):
         self.width = width
         self.height = height
@@ -23,6 +27,14 @@ class SpacialDiffEquation(abc.ABC):
 
     @abc.abstractmethod
     def coordinate_wise_diff_eq(self, t, *concentration_arrays):
+        """
+        The coordinate-wise part of the differential equation.
+        :param t: time at this point in time
+        :param concentration_arrays: tuple of (width,height)-shaped numpy arrays for each particle type containing the
+        concentration of this particle at each cell at this point in time
+        :return: a tuple of (width,height)-shaped numpy arrays for each particle type containing the rate of change
+        of the concentration of this particle particle at each cell at this point in time
+        """
         pass
 
     def solve(self, initial_concentrations, t_end: float, dt: float):
@@ -35,20 +47,29 @@ class SpacialDiffEquation(abc.ABC):
         :return: 4-dimensonal numpy array with dimension 0: time, dimension 1: particle index, dimension 2: x-coordinate
         dimension 3: y-coordinate
         """
+        # vector version
         y0 = np.array(initial_concentrations).reshape(self.chemical_count*self.width*self.height)
+        # an array of time steps to simulate
         time_steps = np.arange(0, ceil(t_end/dt))*dt
 
-        # the differential equation to give to the scipy
+        # the vector differential equation to give to the scipy
         def diff_eq(y, t):
+            # unwrap y
             concentrations_array = y.reshape(self.chemical_count, self.width, self.height)
             concentrations = tuple(concentrations_array[i, :, :] for i in range(self.chemical_count))
+            # initialise an array for the rates of change of the concentratiosn
             dydt = np.zeros((self.chemical_count, self.width, self.height))
+            # add the contribution of the coordinate wise differential equation
             dydt += np.array(self.coordinate_wise_diff_eq(t, *concentrations), dtype=float)
+            # add the contribution of the diffusion
             for i in range(self.chemical_count):
                 padded = np.pad(concentrations[i], pad_width=(self.diffusion_kernel.shape[0]-1)//2, mode='edge')
                 convolved = scipy.signal.fftconvolve(padded, self.diffusion_kernel, mode='valid')
                 dydt[i, :, :] += convolved * (self.diffusion_rates[i] / self.ds**2)
+            # return dydt but in a vector shape
             return dydt.reshape(self.chemical_count*self.width*self.height)
 
+        # solve the vector differential equation
         solution = scipy.integrate.odeint(diff_eq, y0=y0, t=time_steps)
+        # return the results in a nicer shape
         return solution.reshape((len(time_steps), self.chemical_count, self.width, self.height))
