13285792b963b1e57c0923229f3adaf2c0d61fd9
Lasse Veenstra <veenstralasse@gmail.com>
2023-05-08 21:14:04 +0200

    added a function that plots slices of a single particle over time.

--- animations.py
+++ animations.py
@@ -2,7 +2,9 @@ import json
 import os
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import matplotlib
 import numpy as np
+from math import ceil
 
 
 class Visualize:
@@ -23,6 +25,49 @@ class Visualize:
         self.dt = params["dt"]
         self.t_end = params["t_end"]
 
+    def plot_frames(self, filename: str, channel: str, output_particle: int,
+                    min_t: float, max_t: float, num_frames: int):
+        """
+        Visualize the two-dimensional solution by plotting different slide timestamps next to each other
+        :param filename: name of output file
+        :param channel: string name of particle name
+        :param output_particle: integer that indexes the particle
+        :param min_t: time in the simulation to start
+        :param max_t: time in the simulation to stop
+        :param num_frames: total number of frames we want to show
+        """
+        # cap the min_t and max_t
+        max_t = min(self.t_end, max_t)
+        min_t = max(0.0, min_t)
+
+        # retrieve relevant information
+        values = np.swapaxes(self.values, 0, 1)[output_particle]
+        timestamps = np.linspace(min_t, max_t, num_frames)
+        u = np.arange(0, self.width*self.ds, self.ds)
+        v = np.arange(0, self.height*self.ds, self.ds)
+        # initialize the plot
+        num_cols = 5
+        num_rows = ceil(num_frames/5)
+        fig, axs = plt.subplots(num_rows, num_cols, sharex=True, sharey=True, figsize=(num_cols*4, num_rows*4))
+        axs = list(axs.flatten())
+        # initialize the density and normal plots and add a time text
+        for t, ax in zip(timestamps, axs):
+            dens = ax.pcolormesh(u, v, values[round(values.shape[0]*t/self.t_end), :, :], cmap="viridis",
+                                 shading="auto",
+                                 vmin=np.min(values),
+                                 vmax=np.max(values))
+            ax.set_title(f"{channel} on t={round(t, 2)}")
+        fig.subplots_adjust(right=0.8)
+        cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+        norm = matplotlib.colors.Normalize(vmin=np.min(values), vmax=np.max(values))
+        colorscale = matplotlib.cm.ScalarMappable(norm=norm, cmap="viridis")
+        fig.colorbar(colorscale, cax=cbar_ax, ticks=np.linspace(np.min(values), np.max(values), 10))
+        # delete empty plots
+        for ax in axs[num_frames:]:
+            fig.delaxes(ax)
+        # save the plot
+        plt.savefig(filename)
+
     def track_point(self, filename: str, channels: list[str],
                     output_particles: list[int], min_t: float, max_t: float, track_point):
         """
@@ -34,17 +79,18 @@ class Visualize:
         :param max_t: time in the simulation to end the animation
         :param track_point: optional (x, y) coordinates to show over time next to the other plots
         """
+        # cap the min_t and max_t
+        max_t = min(self.t_end, max_t)
+        min_t = max(0.0, min_t)
+
         values = [list(np.swapaxes(self.values, 0, 1))[i] for i in output_particles]
-        if min_t < 0 or max_t > self.t_end:
-            raise ValueError(f"Invalid values for min_t and max_t. Note that max_t"
-                             f" must be smaller or equal to t_end of the simulation")
         # get data for track_point
         u = np.arange(0, self.width*self.ds, self.ds)
         v = np.arange(0, self.height*self.ds, self.ds)
         i_u_nearest = (np.abs(u - track_point[0])).argmin()
         i_v_nearest = (np.abs(v - track_point[1])).argmin()
         track_values = [value[:, i_u_nearest, i_v_nearest] for value in values]
-        num2shape = {"1": (1, 1, 6, 4), "2": (2, 1, 12, 8), "3": (3, 1, 18, 14), "4": (2, 2, 24, 16)}
+        num2shape = {"1": (1, 1, 6, 4), "2": (2, 1, 12, 8), "3": (3, 1, 12, 8), "4": (2, 2, 20, 15)}
         # amount of particles that are shown
         num_particles = len(values)
         # initialize figure and axis
@@ -55,6 +101,7 @@ class Visualize:
         for i, ax in enumerate(axs):
             ax.plot(np.linspace(min_t, max_t, len(track_values[i])), track_values[i], color="black", linewidth=1.2)
             ax.set(xlabel=r"$\hat{t}$", ylabel=channels[i])
+            ax.set_title(f"{channels[i]} over time")
         plt.savefig(filename)
 
     def grid_animation(self, video_frame_rate: int, video_t_per_second, filename: str,
@@ -70,10 +117,11 @@ class Visualize:
         :param max_t: time in the simulation to end the animation
         :param track_point: optional (x, y) coordinates to show over time next to the other plots
         """
+        # cap the min_t and max_t
+        max_t = min(self.t_end, max_t)
+        min_t = max(0.0, min_t)
+
         values = [list(np.swapaxes(self.values, 0, 1))[i] for i in output_particles]
-        if min_t < 0 or max_t > self.t_end:
-            raise ValueError(f"Invalid values for min_t and max_t. Note that max_t"
-                             f" must be smaller or equal to t_end of the simulation")
 
         # create a mesh for the space that we simulate over
         u = np.arange(0, self.width*self.ds, self.ds)
@@ -149,6 +197,33 @@ class Visualize:
         anim.save(filename, writer='ffmpeg', fps=video_frame_rate)
 
 
+def frames_test(test_name):
+    # folder to read from
+    output_folder = "output"
+    # parameters
+    output_file_name = "framesTest"
+    min_t = 0
+    max_t = 30
+    output_format = ".png"
+    output_particle = 3
+    channel = r"$\hat y$"
+    num_frames = 17
+
+    # load data
+    found_folder = None
+    for test_folder in os.listdir(output_folder):
+        if test_folder.startswith(test_name):
+            found_folder = test_folder
+            break
+    output_subfolder = os.path.join(output_folder, found_folder)
+
+    # generate the animation
+    print(f"Creating {found_folder}...")
+    anim = Visualize(output_subfolder)
+    anim.plot_frames(os.path.join(output_subfolder, f"{output_file_name}{output_format}"),
+                     channel, output_particle, min_t, max_t, num_frames)
+
+
 def track_test(test_name):
     # folder to read from
     output_folder = "output"
@@ -170,7 +245,7 @@ def track_test(test_name):
     output_subfolder = os.path.join(output_folder, found_folder)
 
     # generate the animation
-    print(f"Animating {found_folder}...")
+    print(f"Creating {found_folder}...")
     anim = Visualize(output_subfolder)
     anim.track_point(os.path.join(output_subfolder, f"{output_file_name}{output_format}"),
                      channels, output_particles, min_t, max_t, track_point=track_point)
@@ -203,9 +278,9 @@ def animate_test(test_name):
     anim = Visualize(output_subfolder)
     anim.grid_animation(video_frame_rate, video_t_per_second,
                         os.path.join(output_subfolder, f"{output_file_name}{output_format}"),
-                        channels, output_particles, min_t, max_t, track_point=track_point)
+                        channels, output_particles, min_t, max_t, track_point=None)
 
 
 if __name__ == '__main__':
     #animate_test(input("Give the name of the test:\n"))
-    track_test(input("Give the name of the test:\n"))
+    frames_test(input("Give the name of the test:\n"))
